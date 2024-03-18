091a91a37d6782c194304fb56b61bcace2019de4
maksimich182 <66036049+maksimich182@users.noreply.github.com>
2024-03-18 21:31:24 +0300

    Тесты OnesCounter

--- MihStatLibraryTest/OnesCounterTests/OneCounterTest.cs
+++ MihStatLibraryTest/OnesCounterTests/OneCounterTest.cs
@@ -5,6 +5,7 @@ using MihStatLibrary.Histogram;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -43,6 +44,35 @@ namespace MihStatLibraryTest.OnesCounterTests
             FreqHistogram fq = new FreqHistogram(16);
             fq.Calculate(DataFiles.File01010101_131MB);
             Assert.AreEqual(OnesCounter.Calculate(fq), (new FileInfo(DataFiles.File01010101_131MB).Length * Tools.BITS_IN_BYTE) / 2);
+        }
+
+        /// <summary>
+        /// Тест на исключение при вычислении количества единичных бит в гистограмме частот с разными смещением и размерностью:
+        /// 1. Проверяется возникновение исключения при попытке посчитать количество единичных бит на гистограмме частот с разными значениями смещения и размерности
+        /// </summary>
+        [TestMethod]
+        public void CalculateOneInFreqHistogramWithDifferentShiftAndDimTest()
+        {
+            FreqHistogram fq = new FreqHistogram(16, 3);
+            fq.Calculate(DataFiles.File11110000_5MB);
+            Assert.ThrowsException<OnesCounterException>(() => OnesCounter.Calculate(fq));
+        }
+
+        /// <summary>
+        /// Тест на вычисление количества единичных бит данных из <see cref="BigInteger"/>:
+        /// 1. Сравнивается количество единичных бит в числе 987345834572187234912834 = 
+        /// 1101_0001_0001_0100_0001_1111_1111_1100_0011_0000_1111_1001_0111_0000_0011_0010_1100_1010_0100_0010
+        /// с 37 (посчитанно вручную)
+        /// 2. Сравнивается количество единичных бит в первых 30 битах в числа 987345834572187234912834 = 
+        /// 1101_0001_0001_0100_0001_1111_1111_1100_0011_0000_1111_1001_0111_0000_0011_0010_1100_1010_0100_0010
+        /// с 37 (посчитанно вручную)
+        /// </summary>
+        [TestMethod]
+        public void CalculateOneInBigIntegerTest()
+        {
+            BigInteger data = BigInteger.Parse("987345834572187234912834");
+            Assert.AreEqual(OnesCounter.Calculate(data), 37);
+            Assert.AreEqual(OnesCounter.Calculate(data, 30), 11);
         }
     }
 }
